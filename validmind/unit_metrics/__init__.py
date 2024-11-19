89de5d8cf83286a74e3de29339a7451ba73c18ec
John Walz <john@validmind.ai>
2024-11-19 13:30:25 -0500

    refactor: get unit metrics working properly

--- validmind/unit_metrics/__init__.py
+++ validmind/unit_metrics/__init__.py
@@ -3,20 +3,21 @@
 # SPDX-License-Identifier: AGPL-3.0 AND ValidMind Commercial
 
 from validmind.tests._store import test_provider_store
+from validmind.tests.load import describe_test
 from validmind.tests.run import run_test
 
 
-def list_metrics():
+def list_metrics(**kwargs):
     """List all metrics"""
     vm_provider = test_provider_store.get_test_provider("validmind")
     vm_metrics_provider = vm_provider.metrics_provider
 
-    return vm_metrics_provider.list_tests()
+    return vm_metrics_provider.list_tests(**kwargs)
 
 
-def describe_metric(metric_id: str):
+def describe_metric(metric_id: str, **kwargs):
     """Describe a metric"""
-    pass
+    return describe_test(metric_id, **kwargs)
 
 
 def run_metric(metric_id: str, **kwargs):
