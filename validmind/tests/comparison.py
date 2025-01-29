395dc1ab3c76be879bb16ceafb62791b59c7255b
John Walz <john@validmind.ai>
2025-01-29 11:45:57 -0500

    fix: adding raw data support to comparison tests

--- validmind/tests/comparison.py
+++ validmind/tests/comparison.py
@@ -15,7 +15,7 @@ from validmind.vm_models.figure import (
     is_png_image,
 )
 from validmind.vm_models.input import VMInput
-from validmind.vm_models.result import ResultTable, TestResult
+from validmind.vm_models.result import RawData, ResultTable, TestResult
 
 logger = get_logger(__name__)
 
@@ -310,6 +310,25 @@ def get_comparison_test_configs(
         ]
 
     return test_configs
+
+
+def _combine_raw_data(results: List[TestResult]) -> RawData:
+    """Combine RawData objects"""
+    attribute_names = results[0].raw_data.__dict__.keys()
+
+    # check that all the raw data objects have the same attributes
+    for result in results:
+        if not isinstance(result.raw_data, RawData):
+            raise ValueError("All raw data objects must be of type RawData")
+        if result.raw_data.__dict__.keys() != attribute_names:
+            raise ValueError("RawData objects must have the same attributes")
+
+    return RawData(
+        **{
+            key: [getattr(result.raw_data, key) for result in results]
+            for key in attribute_names
+        }
+    )
 
 
 def combine_results(
